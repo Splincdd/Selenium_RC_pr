d05e3e54457a51d0402ac7e0b4341c39160ff5d7
Michal Derevyanko <m.v.derevyanko@gmail.com>
2010-12-15 13:39:37 +0200

    tes
    Signed-off-by: Michal Derevyanko <m.v.derevyanko@gmail.com>

--- src/Login.java
+++ src/Login.java
@@ -54,7 +54,7 @@ public void tearDown() throws Exception {
 	selenium.click("//btnG");
 	
 
-	selenium.type("q", "Best Test");
+	selenium.type("111", "Best Test");
 	selenium.click("//btnG");
 	}
 }
